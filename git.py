0770a34c8d51856d262d4a40928850dcc931e854
YZYpromax <yzy985875994@163.com>
2025-06-01 21:42:50 +0800

    add again

--- git.py
+++ git.py
@@ -1,7 +1,8 @@
 import matplotlib
 class yzy:
-    def __init__(self,year,hight,sex):
+    def __init__(self,year,hight,sex,peersonality):
         self.year =year
         self.hight = hight
         self.sex = sex
+        self.personality =peersonality
         
